21366527b4354c3db0bb7b97ef5916c2c346ec2c
michael dudley <michael.dudley@improvingenterprises.com>
2018-02-17 23:40:01 -0600

    configuring docs

---
+++ doc/conf.py
@@ -0,0 +1,17 @@
+project   = u'Miruken-Specification'
+copyright = u'2014-2018, Miruken'
+author    = u'Craig Neuwirt, Michael Dudley'
+
+sourcesuffix     = '.rst'
+master_doc       = 'index'
+exclude_patterns = ['**/README.rst']
+pygments_style   = 'sphinx'
+
+html_theme      = "sphinx_rtd_theme"
+html_theme_path = ["_themes", ]
+html_theme_options = {
+    'display_version': True,
+    'prev_next_buttons_location': 'both',
+    'collapse_navigation': False,
+    'sticky_navigation': True
+}
